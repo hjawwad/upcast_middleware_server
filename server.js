c1fe429c648acbf78dda089b8cfa26b7a1a1f25d
Jawwad Hussain <hjawwad456@gmail.com>
2023-02-17 15:09:13 +0500

    create middleware to access post video upload api of upcast.work

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://upcast.work",
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Proxy server running on port 3000");
+});
